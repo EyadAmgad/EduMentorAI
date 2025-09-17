52b0548223866a51f94883e5b4f041909abba7aa
EyadAmgad <eyad.mahmoud@ejust.edu.eg>
2025-09-17 17:26:45 +0300

    update the rag cycle

--- src/rag_app/models.py
+++ src/rag_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Document(models.Model):
     title = models.CharField(max_length=255)
-    file = models.FileField(upload_to='files/')
+    file = models.FileField(upload_to='assets/files/')
 
     def __str__(self):
         return self.title
